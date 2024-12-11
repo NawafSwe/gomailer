6d9c29995899fac31323e6d1284d08eadc600268
NawafSwe <nawaf.jr9@hotmail.com>
2024-12-12 00:19:50 +0300

    chore:refactoring

--- message/encoder.go
+++ message/encoder.go
@@ -23,8 +23,6 @@ func splitLines(input string, maxLength int) []string {
 }
 
 // encode encodes mail components into bytes to be sent.
-// TODO: handle alternative use case
-// TODO: when mail should have two parts one plain text and one is html.
 func encode(m Message) []byte {
 	var mailMessage strings.Builder
 	mailSubjectEncoded := "=?UTF-8?B?" + encodeBase64(m.Subject) + "?="
@@ -63,81 +61,76 @@ func encode(m Message) []byte {
 	// if Message has attachement
 	if hasAttachement {
 		mailMessage.WriteString(fmt.Sprintf("--%s%s", boundary, crlf))
-		if hasBothPlainAndHTML {
-			mailMessage.WriteString(fmt.Sprintf("Content-Type: %s%s", multiPartAlternativeContentType, crlf))
-			mailMessage.WriteString(fmt.Sprintf("--%s%s", altBoundary, crlf))
-			// plain text content.
-			mailMessage.WriteString(fmt.Sprintf("Content-Type: %s%s", plainContentType, crlf))
-			mailMessage.WriteString(fmt.Sprintf("Content-Transfer-Encoding: 8bit%s", crlf))
-			mailMessage.WriteString(crlf)
-			for _, line := range splitLines(m.Body, maxLineLength) {
-				mailMessage.WriteString(line + crlf)
-			}
-
-			mailMessage.WriteString(crlf)
-
-			// html content.
-			mailMessage.WriteString(fmt.Sprintf("--%s%s", altBoundary, crlf))
-			mailMessage.WriteString(fmt.Sprintf("Content-Type: %s%s", htmlTypeContentType, crlf))
-			mailMessage.WriteString(fmt.Sprintf("Content-Transfer-Encoding: 8bit%s", crlf))
-			mailMessage.WriteString(crlf)
-			mailMessage.WriteString(m.HTMLBody + crlf)
-			// closing boundary
-			mailMessage.WriteString(fmt.Sprintf("--%s--%s", altBoundary, crlf))
-
-		} else if m.HTMLBody != "" {
-			mailMessage.WriteString(fmt.Sprintf("Content-Type: %s%s", htmlTypeContentType, crlf))
-			mailMessage.WriteString(fmt.Sprintf("Content-Transfer-Encoding: 8bit%s", crlf))
-			mailMessage.WriteString(crlf)
-			mailMessage.WriteString(m.HTMLBody)
-		} else {
-			mailMessage.WriteString(fmt.Sprintf("Content-Type: %s%s", plainContentType, crlf))
-			mailMessage.WriteString(fmt.Sprintf("Content-Transfer-Encoding: 8bit%s", crlf))
-			mailMessage.WriteString(crlf)
-			for _, line := range splitLines(m.Body, maxLineLength) {
-				mailMessage.WriteString(line + crlf)
-			}
+		mailMessage.WriteString(encodeMultiPartMixed(m))
+		// Add attachments
+		for _, attachment := range m.Attachments {
+			mailMessage.WriteString(attachment.encode())
 		}
-		mailMessage.WriteString(crlf)
+		// Final boundary to indicate the end of the message
+		mailMessage.WriteString(fmt.Sprintf("--%s--%s", boundary, crlf))
+
 	} else {
-		if hasBothPlainAndHTML {
-			mailMessage.WriteString(fmt.Sprintf("--%s%s", altBoundary, crlf))
-			// plain text content.
-			mailMessage.WriteString(fmt.Sprintf("Content-Type: %s%s", plainContentType, crlf))
-			mailMessage.WriteString(fmt.Sprintf("Content-Transfer-Encoding: 8bit%s", crlf))
-			mailMessage.WriteString(crlf)
-			for _, line := range splitLines(m.Body, maxLineLength) {
-				mailMessage.WriteString(line + crlf)
-			}
-
-			mailMessage.WriteString(crlf)
-
-			// html content.
-			mailMessage.WriteString(fmt.Sprintf("--%s%s", altBoundary, crlf))
-			mailMessage.WriteString(fmt.Sprintf("Content-Type: %s%s", htmlTypeContentType, crlf))
-			mailMessage.WriteString(fmt.Sprintf("Content-Transfer-Encoding: 8bit%s", crlf))
-			mailMessage.WriteString(crlf)
-			mailMessage.WriteString(m.HTMLBody + crlf)
-			// closing boundary
-			mailMessage.WriteString(fmt.Sprintf("--%s--%s", altBoundary, crlf))
-
-		}
-		if m.HTMLBody != "" {
-			mailMessage.WriteString(m.HTMLBody + crlf)
-		} else {
-			mailMessage.WriteString(crlf)
-			for _, line := range splitLines(m.Body, maxLineLength) {
-				mailMessage.WriteString(line + crlf)
-			}
-		}
-		return []byte(mailMessage.String())
+		mailMessage.WriteString(encodeMessageContent(m))
 	}
-	// Add attachments
-	for _, attachment := range m.Attachments {
-		mailMessage.WriteString(attachment.encode())
-	}
-
-	// Final boundary to indicate the end of the message
-	mailMessage.WriteString(fmt.Sprintf("--%s--%s", boundary, crlf))
 	return []byte(mailMessage.String())
+}
+
+// encodeMessageContent function encodes the Message.Body, and Message.HTMLBody.
+func encodeMessageContent(m Message) string {
+	var mb strings.Builder
+	// check if mail has both versions.
+	if m.Body != "" && m.HTMLBody != "" {
+		mb.WriteString(fmt.Sprintf("Content-Type: %s%s", multiPartAlternativeContentType, crlf))
+		mb.WriteString(fmt.Sprintf("--%s%s", altBoundary, crlf))
+		// Plain text content.
+		mb.WriteString(fmt.Sprintf("Content-Type: %s%s", plainContentType, crlf))
+		mb.WriteString(fmt.Sprintf("Content-Transfer-Encoding: 8bit%s", crlf))
+		mb.WriteString(crlf)
+		for _, line := range splitLines(m.Body, maxLineLength) {
+			mb.WriteString(line + crlf)
+		}
+
+		mb.WriteString(crlf)
+		// HTML content.
+
+		mb.WriteString(fmt.Sprintf("--%s%s", altBoundary, crlf))
+		mb.WriteString(fmt.Sprintf("Content-Type: %s%s", htmlTypeContentType, crlf))
+		mb.WriteString(fmt.Sprintf("Content-Transfer-Encoding: 8bit%s", crlf))
+		mb.WriteString(crlf)
+		mb.WriteString(m.HTMLBody + crlf)
+		// Closing boundary
+		mb.WriteString(fmt.Sprintf("--%s--%s", altBoundary, crlf))
+	} else if m.HTMLBody != "" {
+		mb.WriteString(m.HTMLBody + crlf)
+	} else {
+		for _, line := range splitLines(m.Body, maxLineLength) {
+			mb.WriteString(line + crlf)
+		}
+	}
+
+	return mb.String()
+}
+
+// encodeMultiPartMixed function encodes multipart mixed and encodeMessageContent if any.
+func encodeMultiPartMixed(m Message) string {
+	var mb strings.Builder
+	// check if mail has content as alternative
+	if m.HTMLBody != "" && m.Body != "" {
+		mb.WriteString(encodeMessageContent(m))
+	} else if m.HTMLBody != "" {
+		mb.WriteString(fmt.Sprintf("Content-Type: %s%s", htmlTypeContentType, crlf))
+		mb.WriteString(fmt.Sprintf("Content-Transfer-Encoding: 8bit%s", crlf))
+		mb.WriteString(crlf)
+		mb.WriteString(m.HTMLBody)
+	} else {
+		mb.WriteString(fmt.Sprintf("Content-Type: %s%s", plainContentType, crlf))
+		mb.WriteString(fmt.Sprintf("Content-Transfer-Encoding: 8bit%s", crlf))
+		mb.WriteString(crlf)
+		for _, line := range splitLines(m.Body, maxLineLength) {
+			mb.WriteString(line + crlf)
+		}
+	}
+	mb.WriteString(crlf)
+
+	return mb.String()
 }
